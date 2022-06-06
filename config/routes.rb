af2ab4028813a93f7fb1ae16d7b37c1562354a1b
Kensuke Asao <a.knsk0718@gmail.com>
2022-06-06 12:25:20 +0000

    add static three pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
@@ -6,6 +7,4 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'application#hello'
-  # root 'users#index'
-  root 'application#hey'
 end
