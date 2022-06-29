51ff9ab4e57b29050d99dbaf4e31f9c9bf21d3dc
Kensuke Asao <a.knsk0718@gmail.com>
2022-06-29 12:14:47 +0000

    add new and create methods in user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,26 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user
+      flash[:success] = 'Welcome to the Sample App!'
+    else
+      logger.info @user.errors.full_messages
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
