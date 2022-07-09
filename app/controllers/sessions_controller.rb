a83db2c19a4b8d9ec959d4b8f8f9fe94a132da77
Kensuke Asao <a.knsk0718@gmail.com>
2022-07-09 06:36:37 +0000

    refactoring

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,13 +2,14 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user&.authenticate(params[:session][:password])
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user&.authenticate(params[:session][:password])
       # ログイン後のページにリダイレク
-      log_in(user)
-      remember(user)
-      # 自動的に user_url(user) -> users/:id に変換している
-      redirect_to user
+      log_in(@user)
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      Rails.logger.info params[:session][:remember_me]
+      # 自動的に user_url(@user) -> users/:id に変換している
+      redirect_to @user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
