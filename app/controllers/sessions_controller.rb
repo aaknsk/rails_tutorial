4c3e976ad250d4ba8b852e94c716cbdbfec017f8
Kensuke Asao <a.knsk0718@gmail.com>
2022-07-09 05:36:24 +0000

    複数ブラウザ利用時、片方ログアウトした際発生するエラーの防止

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user && user&.authenticate(params[:session][:password])
       # ログイン後のページにリダイレク
       log_in(user)
       remember(user)
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -41,6 +41,8 @@ class User < ApplicationRecord
   # 渡されたトークンがダイジェストと一致したらtrueを返す, このremember_tokenはクラス変数とは別物の引数
   # ここで使用している remember_digestはUsersテーブル内のカラム
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
+
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
